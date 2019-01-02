cf1ed5ece0c13aeff804a1cd2cd498f4ee43778e
pengte <18373870420@163.com>
2019-01-02 19:07:35 +0800

    DEwang

--- src/main/java/com/zking/de/controller/billing/Dome1.java
+++ src/main/java/com/zking/de/controller/billing/Dome1.java
@@ -16,7 +16,7 @@ public class Dome1 {
 
     @RequestMapping("add")
     public String add(){
-        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaaaaa");
         File file = new File();
         file.setFileId("1");
         System.out.println(file.getFileId());
